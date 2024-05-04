de35483e8624719aff4f52c8f3c84d1cba803996
d1a2r3k4 <149410065+d1a2r3k4@users.noreply.github.com>
2024-05-04 20:33:34 +0400

    Add files via upload

---
+++ ceramic.js
@@ -0,0 +1,53 @@
+
+
+$(document).ready(function(){
+    $(".list").click(function(){
+      $(".appear").css("display","flex");
+    });
+
+    $(".xx").click(function(){
+        $(".appear").css("display","none");
+      });
+
+      
+
+      $("#ff").click(function(){
+        $("#fff").toggle();
+       
+     
+        
+
+      });
+
+      $("#ss").click(function(){
+        $("#sss").toggle();
+      });
+
+      $("#partnior").click(function(){
+        $("#part").toggle();
+      });
+
+
+
+
+
+      // $("#britan").click(function(){
+      //   $("#georgia").css("display","block");
+       
+      //   $(this).css("display","none");
+
+      // });
+
+      // $("#georgia").click(function(){
+      //   $("#britan").css("display","block");
+      
+      //   $(this).css("display","none");
+      // });
+
+
+      
+
+
+
+
+  });
